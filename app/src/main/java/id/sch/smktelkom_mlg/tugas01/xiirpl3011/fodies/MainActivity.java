bc982b72966e2f89ab8a6b15697bbd34426ba87a
Dhita Shandy Faristyawati <dhitashandy12@gmail.com>
2016-10-09 21:55:21 -1200

    menambah script error ketika nama pemesan belum diisi

--- app/src/main/java/id/sch/smktelkom_mlg/tugas01/xiirpl3011/fodies/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/tugas01/xiirpl3011/fodies/MainActivity.java
@@ -2,6 +2,7 @@ package id.sch.smktelkom_mlg.tugas01.xiirpl3011.fodies;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -23,8 +24,46 @@ public class MainActivity extends AppCompatActivity {
         bOK = (Button) findViewById(R.id.buttonOK);
         tvHasil = (TextView) findViewById(R.id.textViewHasil);
 
+        bOK.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                doProcess();
+            }
 
 
+        });
 
+        findViewById(R.id.buttonOK).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                doProcess();
+            }
+        });
+
+    }
+
+    private void doProcess() {
+
+        if (isValid()) {
+            String nama = Nama.getText().toString();
+
+            tvHasil.setText(" Nama Pemesan : " + nama);
+        }
+
+
+    }
+
+    private boolean isValid() {
+        boolean valid = true;
+
+        String nama = Nama.getText().toString();
+        if (nama.isEmpty()) {
+            Nama.setError("Nama Harus Diisi");
+        } else {
+            Nama.setError(null);
+        }
+
+        return valid;
     }
 }
