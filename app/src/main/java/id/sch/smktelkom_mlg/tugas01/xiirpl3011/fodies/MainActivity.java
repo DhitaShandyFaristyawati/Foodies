790f4135b65538bcb6b08e29451a567829e36d44
Dhita Shandy Faristyawati <dhitashandy12@gmail.com>
2016-10-09 21:43:49 -1200

    membuat editText nama pemesan

--- app/src/main/java/id/sch/smktelkom_mlg/tugas01/xiirpl3011/fodies/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/tugas01/xiirpl3011/fodies/MainActivity.java
@@ -2,12 +2,29 @@ package id.sch.smktelkom_mlg.tugas01.xiirpl3011.fodies;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText Nama;
+
+    Button bOK;
+    TextView tvHasil;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Nama = (EditText) findViewById(R.id.editTextNama);
+
+        bOK = (Button) findViewById(R.id.buttonOK);
+        tvHasil = (TextView) findViewById(R.id.textViewHasil);
+
+
+
+
     }
 }
